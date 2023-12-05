52b629500bebddce6ed164f973f12c72193e67fb
massndongo86@gmail.com <massndongo86@gmail.com>
2023-12-05 13:10:58 +0000

    Page CV | Remplissage informations

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HamburgerComponent } from './hamburger/hamburger.component';
 import { MenuComponent } from './menu/menu.component';
@@ -12,53 +10,17 @@ import { MenuComponent } from './menu/menu.component';
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss'],
     host: {
-        'class': 'sticky top-0 w-full z-20'
+        'class': 'sticky top-0 w-full z-20',
+        'aria-controls': 'menu-container'
     },
     imports: [CommonModule, HamburgerComponent, MenuComponent, RouterLink, RouterLinkActive]
 })
-export class NavbarComponent implements OnInit, OnDestroy {
-
-  token: any
-
-
-  currencyForm = new FormGroup({
-    token: new FormControl(),
-    signature: new FormControl(),
-    currency: new FormControl(),
-  })
-
-  profilForm = new FormGroup({
-    profil: new FormControl()
-  })
-
-  pseudo!: string | null;
-  devise: any;
-  val!: string
-
-  isListProfil: any;
-  isListDevise: any;
-  datatauser: any
-  private unsubscription$ = new Subject<void>();
-
-  constructor(
-    private router: Router
-  ) {
-
-
-  }
-
+export class NavbarComponent implements OnInit {
   ngOnInit(): void {
-    this.token="null"
   }
 
 
 
-  ngOnDestroy(): void {
-    if (this.unsubscription$) {
-      this.unsubscription$.next();
-      this.unsubscription$.unsubscribe();
-    }
-  }
 }
 
 
