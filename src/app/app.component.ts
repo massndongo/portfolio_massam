85a0d0a78c60b824313817c53330389f71dbe40c
massndongo86@gmail.com <massndongo86@gmail.com>
2023-12-05 16:06:51 +0000

    Page Contact | Link avec Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { FooterComponent } from './components/footer/footer.component';
       RouterLink,
       RouterLinkActive,
       NavbarComponent,
-      FooterComponent
+      FooterComponent,
     ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD4dg-KszPu-hX8mR46f7zncv6BMavpzC8",
+    authDomain: "portfolio-82140.firebaseapp.com",
+    projectId: "portfolio-82140",
+    storageBucket: "portfolio-82140.appspot.com",
+    messagingSenderId: "121519730345",
+    appId: "1:121519730345:web:30a781e29d8273f3d9c3b8",
+    measurementId: "G-GN6P9S6WKT"
+  }
+};
